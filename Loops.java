543c97480519eb39a47ddbaa5fa102b70d48d4b4
heqiqetrzayeva <heqiqetrzayeva1999@gmail.com>
2023-12-17 18:34:28 +0400

    first commit

---
+++ Loops.java
@@ -0,0 +1,92 @@
+package com.mycompany.loops;
+import java.util.Scanner;
+public class Loops {
+    public static void main(String[] args) {
+        //Task 1. Bir string verilib hemin stringi eksine cevirin Salam - malas:
+//       String text = "Salam";
+//       String reservedText = "";
+
+//        for (int i = text.length()-1; i>=0; i--) {
+//            reservedText += text.charAt(i);
+//        }
+//        System.out.println(reservedText.toLowerCase());
+       
+
+        //Task 2. Adlardan ibaret string arrayi verilib hemin arrayde herflerinin sayi en cox olan adi tapin:
+//            String[] names = {"nergiz","leman","cabir","nigar","meherrem","melek"};
+//            String longestName = "";
+//            			                 
+//		for (String name : names) { 
+//			String[] word = name.split(" "); 
+//                    for (String word1 : word) {
+//                        if (word1.length() >= longestName.length()) {
+//                            longestName = word1;
+//                        }
+//                    } 
+//		} 
+//		System.out.println(longestName); 
+
+                
+        //Task 3. 25 ve 94 ededleri verilib hemin ededler arasinda 2e bolunenlerinin cemini tapin:
+//               int sum = 0;
+
+//               for (int i = 25; i <= 94; i++) {
+//                   if (i%2 == 0) {
+//                       sum += i;
+//                   }
+//        }
+//          System.out.println(sum);
+          
+
+        //Task 4. a ve b ededleri verilib, a-nin b quvvetini tapin:
+//            Scanner myObj = new Scanner(System.in);
+//            
+//            System.out.print("Enter first number: ");
+//            int num1 = myObj.nextInt();
+//            
+//            System.out.print("Enter second number: ");
+//            int num2 = myObj.nextInt();
+//            
+//            int result = (int)Math.pow(num1, num2);
+//            System.out.println(result);
+            
+
+       //Task 5. 2nin quvveti olan bir tam eded verilib, bu ededinin 2nin necenci quvvetini oldugunu tapin:
+//              Scanner myObj = new Scanner(System.in);
+//             
+//              System.out.print("Enter number: ");
+//              int num = myObj.nextInt();
+//              int power = (int)(Math.log(num)/Math.log(2));
+//              if (!(num%2 == 0)) {
+//                  System.out.println("Enter a number that is a power of 2!");                 
+//        }
+//              else{
+//                  System.out.println(power);
+//              }
+
+
+       //Task 6. Ededlerden ibaret array verilib,hemin arrayin ededlerinin ededi ortasini tapin :
+//              int[] arr = {3,6,7,32,54,87};
+//              int sum = 0;
+//              int avg;
+//        
+//              for (int i = 0; i < arr.length; i++) {
+//              sum+=arr[i];
+//        }
+//              avg = sum/arr.length;
+//        
+//              System.out.println(sum);
+//              System.out.println(avg);
+//       
+        
+}
+}    
+        
+    
+        
+
+        
+            
+    
+    
+
